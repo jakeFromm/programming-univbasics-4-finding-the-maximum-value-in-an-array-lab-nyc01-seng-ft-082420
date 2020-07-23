65069c394cdbbbe3eae8b69fd25c06e7471eb8cd
Jacob Fromm <jacobfromm@gmail.com>
2020-07-23 15:59:50 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,6 +2,10 @@ def find_max_value(array)
 max_value = 0
 i = 0
   while i < array.length do
-  
-  end  
+    if array[i] >= max_value
+      max_value = array[i]
+    end
+    i += 1 
+  end 
+  max_value
 end
